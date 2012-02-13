2c49225786c885b6f1368b967ead80bfa747c34c
Daniele De Francesco <ddefrancesco@gmail.com>
2012-02-13 01:07:36 +0100

    *Refactoring della classe ColumnFamilyFactory *HelenusMainTest

--- helenus/src/main/java/org/obiwan/helenus/core/factory/ColumnFamilyFactory.java
+++ helenus/src/main/java/org/obiwan/helenus/core/factory/ColumnFamilyFactory.java
@@ -30,103 +30,25 @@ public class ColumnFamilyFactory implements Serializable {
 	
 
 	
-	public static ColumnFamily createColumnFamily(String familyName, String[] columnNames,Value<?>[] valueClazzes, boolean isSuperColumn) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException{
+	public static ColumnFamily<?> createColumnFamily(String familyName, String[] columnNames,Value<?>[] valueClazzes, boolean isSuperColumn) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException{
 		
-		//Supporta l'annotation xké i parametri sono passati come array di tipi supportati, 
+		//Supporta l'annotation xkŽ i parametri sono passati come array di tipi supportati, 
 		//basta solo che allo stesso indice corrispondano nome e valori passati
 		
-		ColumnFamily columnFamily = new ColumnFamily();
+		ColumnFamily<?> columnFamily = new ColumnFamily(){};
 		columnFamily.setFamily(familyName);
-		Map<String, Map<Integer, ColumnOrSuperColumn>> dataMap =
-				new LinkedHashMap<String, Map<Integer,ColumnOrSuperColumn>>();
+		Map<String, Map<Integer, ColumnOrSuperColumn<?>>> dataMap =
+				new LinkedHashMap<String, Map<Integer,ColumnOrSuperColumn<?>>>();
 		
 		RowKey uid = new RowKey();
 		
-		Map<Integer,ColumnOrSuperColumn> colMap = new LinkedHashMap<Integer, ColumnOrSuperColumn>();;
+		Map<Integer,ColumnOrSuperColumn<?>> colMap = new LinkedHashMap<Integer, ColumnOrSuperColumn<?>>();;
 		Column col = null;
 		for(int i = 0 ; i < columnNames.length; i++){
 			if(!isSuperColumn){
 				ColumnOrSuperColumn<Column> cos = new ColumnOrSuperColumn<Column>();
-				
 				String name = columnNames[i];
-				Value<?> clazz = valueClazzes[i];
-				
-				
-//				Value<?> valClazz = null;
-//				@SuppressWarnings("unchecked")
-//				Constructor<?>[] constructors = (Constructor<?>[]) clazz.getDeclaredConstructors();
-//				Class[] parameterTypes = new Class[4];
-//				Constructor<?> _constructor = null;
-//				
-//				for(Constructor<?> constructor : constructors){
-//					
-//					parameterTypes = constructor.getParameterTypes();
-//					for(int j = 0;j<parameterTypes.length;j++){
-//						//System.out.println("Size di parameterTypes= "+parameterTypes.length);
-//						if(parameterTypes.length == 1){
-//							//_constructor = constructor;
-//							_constructor = clazz.getConstructor(parameterTypes[j]);
-//							
-//							if(parameterTypes[j] == String.class){
-//								
-//								valClazz = new Value<String>() ;
-//								valClazz.setData(new String("").getClass());
-//								valClazz.setTimestamp(System.nanoTime());	
-//								
-//							}
-//							else if(parameterTypes[j] == Long.TYPE){
-//								
-//								valClazz = new Value<Long>();
-//								valClazz.setData(new Long(0).getClass());
-//								valClazz.setTimestamp(System.nanoTime());	
-//							}
-//								
-//							else if(parameterTypes[j] == Float.TYPE){
-//								
-//								valClazz = new Value<Float>();
-//								valClazz.setData(new Float(0).getClass());
-//								valClazz.setTimestamp(System.nanoTime());	
-//							}
-//							else if(parameterTypes[j] == Double.TYPE){
-//								
-//								valClazz = new Value<Double>();
-//								valClazz.setData(new Double(0).getClass());
-//								valClazz.setTimestamp(System.nanoTime());	
-//							}
-//							else if(parameterTypes[j] == Boolean.TYPE){
-//								
-//								valClazz = new Value<Boolean>();
-//								valClazz.setData(new Boolean(false).getClass());
-//								valClazz.setTimestamp(System.nanoTime());	
-//							}
-//							else if(parameterTypes[j] == Integer.TYPE){
-//								
-//								valClazz = new Value<Integer>();
-//								valClazz.setData(new Integer(0).getClass());
-//								valClazz.setTimestamp(System.nanoTime());	
-//							}
-//							else if (parameterTypes[j] == BigInteger.class) {
-//
-//								valClazz = new Value<BigInteger>();
-//								valClazz.setData(new BigInteger("0").getClass());
-//								valClazz.setTimestamp(System.nanoTime());
-//							} 
-//							else if (parameterTypes[j] == BigDecimal.class) {
-//
-//								valClazz = new Value<BigDecimal>();
-//								valClazz.setData(new BigDecimal(0).getClass());
-//								valClazz.setTimestamp(System.nanoTime());
-//							}
-//						
-//							break;
-//						}
-//					
-//					}
-
-	//			}
 				for(Value<?> valClazz : valueClazzes){
-					
-					
 					col = new Column(name,valClazz){};	
 					cos.setColumn(col);
 				}
--- helenus/src/test/java/org/obiwan/helenus/core/HelenusTestMain.java
+++ helenus/src/test/java/org/obiwan/helenus/core/HelenusTestMain.java
@@ -3,10 +3,14 @@
  */
 package org.obiwan.helenus.core;
 
-import org.obiwan.helenus.core.CassandraConnection;
+import java.lang.reflect.InvocationTargetException;
+
 import org.obiwan.helenus.core.annotation.ConnectionData;
 import org.obiwan.helenus.core.exception.AnnotationNotPresentException;
+import org.obiwan.helenus.core.factory.ColumnFamilyFactory;
 import org.obiwan.helenus.core.factory.ConnectionFactory;
+import org.obiwan.helenus.datamodel.ColumnFamily;
+import org.obiwan.helenus.datamodel.Value;
 
 /**
  * @author ddefrancesco
@@ -23,9 +27,27 @@ public class HelenusTestMain {
 		
 		try {
 			CassandraConnection connection = ConnectionFactory.initInstance(HelenusTestMain.class);
+			String[] columnNames = {"name","age","email"};
+			Value<?>[] valueTypes = new Value<?>[3];
+			valueTypes[0] = new Value<String>();
+			valueTypes[1] = new Value<Integer>();
+			valueTypes[2] = new Value<String>();
+			ColumnFamily<?> columnFamily = ColumnFamilyFactory.createColumnFamily(cf, columnNames, valueTypes, false);
 			
 			connection.shutdownConnectionPool();
 		} catch (AnnotationNotPresentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
